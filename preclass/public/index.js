201f3727ea69daebb25d426d733343008481a557
Erick Wendel <erick.workspace@gmail.com>
2023-06-15 22:06:32 -0300

    finish script
    Signed-off-by: Erick Wendel <erick.workspace@gmail.com>

--- preclass/public/index.js
+++ preclass/public/index.js
@@ -4,7 +4,7 @@ function sum(var1, var2) {
 
 function info(name) {
     const age = sum(10, 18);
-    const profession = 'Software Engineer'
+    const profession = 'profession'
     const message = `Hello, ${name}. You are ${age} years old and your profession is ${profession}!`
     console.log(message)
 }
